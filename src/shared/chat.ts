bc2ee0be3eee514b89013db4cada0fe54ecd861b
Suno <ilovesuno@proton.me>
2024-10-12 14:20:33 -0400

    Add more `chat` functions

--- src/shared/chat.ts
+++ src/shared/chat.ts
@@ -1,20 +1,56 @@
 // Declare Global Chat-Based Functions
 
-export function chat(message: string) {
-    const replicated_storage = game.GetService('ReplicatedStorage');
-    const text_chat = game.GetService('TextChatService');
+interface Chats {
+    chat(data: {}): void,
+    handle(): void
+}
 
-    if (replicated_storage.FindFirstChild('DefaultChatSystemChatEvents')) {
-        const folder = replicated_storage.FindFirstChild('DefaultChatSystemChatEvents') as Folder;
-        const event = folder?.FindFirstChild('SayMessageRequest') as RemoteEvent;
+// Constants
 
-        event.FireServer(message, 'All');
-    } else {
-        const text_channel = text_chat.FindFirstChild('TextChannels');
-        const main = text_channel?.FindFirstChild('RBXGeneral');
+const replicated_storage = game.GetService('ReplicatedStorage');
+const text_chat = game.GetService('TextChatService');
+const players = game.GetService('Players');
 
-        if (main) {
-            (main as TextChannel).SendAsync(message);
+const local_player = players.LocalPlayer as Player;
+
+const chat_type = text_chat.ChatVersion;
+
+const chats: Chats = {
+    /* Makes Your Player Chat for You */
+
+    chat: function(message: string) {
+        if (chat_type === Enum.ChatVersion.LegacyChatService) {
+            const folder = replicated_storage.FindFirstChild('DefaultChatSystemChatEvents') as Folder;
+            const event = folder?.FindFirstChild('SayMessageRequest') as RemoteEvent;
+
+            event.FireServer(message, 'All');
+        } else {
+            const text_channel = text_chat.FindFirstChild('TextChannels');
+            const main = text_channel?.FindFirstChild('RBXGeneral');
+
+            if (main) {
+                (main as TextChannel).SendAsync(message);
+            }
+        }
+    },
+
+    /* Listens & Returns Messages You Send */
+
+    handle: function() {
+        if (chat_type === Enum.ChatVersion.LegacyChatService) {
+            local_player.Chatted.Connect(function(message: string) {
+                return message;
+            })
+        } else {
+            text_chat.MessageReceived.Connect(function(message: TextChatMessage) {
+                const author = message.TextSource as unknown;
+
+                if (author === local_player.Name) {
+                    return message;
+                }
+            })
         }
     }
 }
+
+export default chats;
